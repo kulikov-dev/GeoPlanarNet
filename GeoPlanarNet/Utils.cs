dded98ac3601e2529163d5f33e21b8ff01054230
kulikov-dev <kulikov.dev@gmail.com>
2022-12-08 15:52:50 +0300

    First commit, part of utils

---
+++ GeoPlanarNet/Utils.cs
@@ -0,0 +1,28 @@
+﻿namespace GeoPlanarNet
+{
+    /// <summary>
+    /// Supporting methods for library
+    /// </summary>
+    internal static class Utils
+    {
+        /// <summary>
+        /// Convert radian to degree
+        /// </summary>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Angle in radians </returns>
+        public static double ConvertRadianToDegree(double angleRadian)
+        {
+            return angleRadian * 180 / Math.PI;
+        }
+
+        /// <summary>
+        /// Convert degree to rad
+        /// </summary>
+        /// <param name="angleDegree"> Angle in degree </param>
+        /// <returns> Angle in degree </returns>
+        public static double ConvertDegreeToRad(double angleDegree)
+        {
+            return angleDegree * Math.PI / 180;
+        }
+    }
+}

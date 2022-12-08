5c9c29abf31ef605d2964d4ee2932c9d5cbd0f83
kulikov-dev <kulikov.dev@gmail.com>
2022-12-08 16:29:17 +0300

    Added area of triangle, distance between points, rotate point around point.

---
+++ GeoPlanarNet/PointGeo.cs
@@ -0,0 +1,173 @@
+﻿using System.Drawing;
+
+namespace GeoPlanarNet
+{
+    /// <summary>
+    /// Class to process operations with points
+    /// </summary>
+    public static class PointGeo
+    {
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1"> Point 1 </param>
+        /// <param name="point2"> Point 2</param>
+        /// <returns> Distance </returns>
+        public static float Distance(PointF point1, PointF point2)
+        {
+            return Distance(point1.X, point1.Y, point2.X, point2.Y);
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1"> Point 1 </param>
+        /// <param name="point2"> Point 2</param>
+        /// <returns> Distance </returns>
+        public static float DistanceTo(this PointF point1, PointF point2)
+        {
+            return Distance(point1, point2);
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1"> Point 1 </param>
+        /// <param name="point2"> Point 2</param>
+        /// <returns> Distance </returns>
+        public static double Distance(Point point1, Point point2)
+        {
+            return Distance(point1.X, point1.Y, point2.X, point2.Y);
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1"> Point 1 </param>
+        /// <param name="point2"> Point 2</param>
+        /// <returns> Distance </returns>
+        public static double DistanceTo(this Point point1, Point point2)
+        {
+            return Distance(point1, point2);
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1X"> Point 1: X coordinate </param>
+        /// <param name="point1Y"> Point 1: Y coordinate </param>
+        /// <param name="point2X"> Point 2: X coordinate </param>
+        /// <param name="point2Y"> Point 2: Y coordinate </param>
+        /// <returns> Distance </returns>
+        public static float Distance(float point1X, float point1Y, float point2X, float point2Y)
+        {
+            return (float)Math.Sqrt(Math.Pow(point1X - point2X, 2) + Math.Pow(point1Y - point2Y, 2));
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1X"> Point 1: X coordinate </param>
+        /// <param name="point1Y"> Point 1: Y coordinate </param>
+        /// <param name="point2X"> Point 2: X coordinate </param>
+        /// <param name="point2Y"> Point 2: Y coordinate </param>
+        /// <returns> Distance </returns>
+        public static double Distance(double point1X, double point1Y, double point2X, double point2Y)
+        {
+            return Math.Sqrt(Math.Pow(point1X - point2X, 2) + Math.Pow(point1Y - point2Y, 2));
+        }
+
+        /// <summary>
+        /// Get distance between two points
+        /// </summary>
+        /// <param name="point1X"> Point 1: X coordinate </param>
+        /// <param name="point1Y"> Point 1: Y coordinate </param>
+        /// <param name="point2X"> Point 2: X coordinate </param>
+        /// <param name="point2Y"> Point 2: Y coordinate </param>
+        /// <returns> Distance </returns>
+        public static double Distance(int point1X, int point1Y, int point2X, int point2Y)
+        {
+            return Math.Sqrt(Math.Pow(point1X - point2X, 2) + Math.Pow(point1Y - point2Y, 2));
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="point"> Source point </param>
+        /// <param name="center"> Center point </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static PointF Rotate(PointF point, PointF center, double angleRadian)
+        {
+            return Rotate(point.X, point.Y, center.X, center.Y, angleRadian);
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="point"> Source point </param>
+        /// <param name="center"> Center point </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static PointF RotateAround(this PointF point, PointF center, double angleRadian)
+        {
+            return Rotate(point, center, angleRadian);
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="point"> Source point </param>
+        /// <param name="center"> Center point </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static Point Rotate(Point point, Point center, double angleRadian)
+        {
+            return Rotate(point.X, point.Y, center.X, center.Y, angleRadian);
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="point"> Source point </param>
+        /// <param name="center"> Center point </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static Point RotateAround(this Point point, Point center, double angleRadian)
+        {
+            return Rotate(point, center, angleRadian);
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="pointX">Point: X coordinate </param>
+        /// <param name="pointY"> Point: Y coordinate </param>
+        /// <param name="centerX"> Center point: X coordinate </param>
+        /// <param name="centerY"> Center point: Y coordinate </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static PointF Rotate(float pointX, float pointY, float centerX, float centerY, double angleRadian)
+        {
+            var diff = new PointF(pointX - centerX, pointY - centerY);
+
+            return new PointF((float)(centerX + (diff.X * Math.Cos(angleRadian)) - (diff.Y * Math.Sin(angleRadian))), (float)(centerY + (diff.X * Math.Sin(angleRadian)) + (diff.Y * Math.Cos(angleRadian))));
+        }
+
+        /// <summary>
+        /// Rotate point around the center
+        /// </summary>
+        /// <param name="pointX">Point: X coordinate </param>
+        /// <param name="pointY"> Point: Y coordinate </param>
+        /// <param name="centerX"> Center point: X coordinate </param>
+        /// <param name="centerY"> Center point: Y coordinate </param>
+        /// <param name="angleRadian"> Angle in radians </param>
+        /// <returns> Rotated point </returns>
+        public static Point Rotate(int pointX, int pointY, int centerX, int centerY, double angleRadian)
+        {
+            var diff = new Point(pointX - centerX, pointY - centerY);
+
+            return new Point((int)(centerX + (diff.X * Math.Cos(angleRadian)) - (diff.Y * Math.Sin(angleRadian))), (int)(centerY + (diff.X * Math.Sin(angleRadian)) + (diff.Y * Math.Cos(angleRadian))));
+        }
+    }
+}
---
+++ GeoPlanarNet/TriangleGeo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoPlanarNet
+{
+    public static class TriangleGeo
+    {
+        /// <summary>
+        /// Get triangle area
+        /// </summary>
+        /// <param name="apex1"> Apex 1 </param>
+        /// <param name="apex2"> Apex 2 </param>
+        /// <param name="apex3"> Apex 3 </param>
+        /// <returns> Triangle area </returns>
+        private static double Area(PointF apex1, PointF apex2, PointF apex3)
+        {
+            return Area(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
+        }
+
+        /// <summary>
+        /// Get triangle area
+        /// </summary>
+        /// <param name="apex1"> Apex 1 </param>
+        /// <param name="apex2"> Apex 2 </param>
+        /// <param name="apex3"> Apex 3 </param>
+        /// <returns> Triangle area </returns>
+        private static double Area(Point apex1, Point apex2, Point apex3)
+        {
+            return Area(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
+        }
+
+        /// <summary>
+        /// Get triangle area
+        /// </summary>
+        /// <param name="apex1X"> Apex 1: X coordinate </param>
+        /// <param name="apex1Y"> Apex 1: Y coordinate </param>
+        /// <param name="apex2X"> Apex 2: X coordinate </param>
+        /// <param name="apex2Y"> Apex 2: Y coordinate </param>
+        /// <param name="apex3X"> Apex 3: X coordinate </param>
+        /// <param name="apex3Y"> Apex 3: Y coordinate </param>
+        /// <returns> Triangle area </returns>
+        private static float Area(float apex1X, float apex1Y, float apex2X, float apex2Y, float apex3X, float apex3Y)
+        {
+            return ((apex2X - apex1X) * (apex3Y - apex1Y)) - ((apex2Y - apex1Y) * (apex3X - apex1X));
+        }
+
+        /// <summary>
+        /// Get triangle area
+        /// </summary>
+        /// <param name="apex1X"> Apex 1: X coordinate </param>
+        /// <param name="apex1Y"> Apex 1: Y coordinate </param>
+        /// <param name="apex2X"> Apex 2: X coordinate </param>
+        /// <param name="apex2Y"> Apex 2: Y coordinate </param>
+        /// <param name="apex3X"> Apex 3: X coordinate </param>
+        /// <param name="apex3Y"> Apex 3: Y coordinate </param>
+        /// <returns> Triangle area </returns>
+        private static double Area(double apex1X, double apex1Y, double apex2X, double apex2Y, double apex3X, double apex3Y)
+        {
+            return ((apex2X - apex1X) * (apex3Y - apex1Y)) - ((apex2Y - apex1Y) * (apex3X - apex1X));
+        }
+
+        /// <summary>
+        /// Get triangle area
+        /// </summary>
+        /// <param name="apex1X"> Apex 1: X coordinate </param>
+        /// <param name="apex1Y"> Apex 1: Y coordinate </param>
+        /// <param name="apex2X"> Apex 2: X coordinate </param>
+        /// <param name="apex2Y"> Apex 2: Y coordinate </param>
+        /// <param name="apex3X"> Apex 3: X coordinate </param>
+        /// <param name="apex3Y"> Apex 3: Y coordinate </param>
+        /// <returns> Triangle area </returns>
+        private static int Area(int apex1X, int apex1Y, int apex2X, int apex2Y, int apex3X, int apex3Y)
+        {
+            return ((apex2X - apex1X) * (apex3Y - apex1Y)) - ((apex2Y - apex1Y) * (apex3X - apex1X));
+        }
+    }
+}

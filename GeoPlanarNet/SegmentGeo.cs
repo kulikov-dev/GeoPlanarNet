461cf3112ad6a0b84e1196d0ab9939e33e941e4c
kulikov-dev <kulikov.dev@gmail.com>
2022-12-09 16:19:02 +0300

    Get angle between segments

--- GeoPlanarNet/SegmentGeo.cs
+++ GeoPlanarNet/SegmentGeo.cs
@@ -50,7 +50,7 @@ namespace GeoPlanarNet
         }
 
         /// <summary>
-        /// Get angle in radians between two segments: (commonPoint, startPoint1) and (commonPoint, startPoint2)
+        /// Get angle in radians between two segments with a common point: (commonPoint, startPoint1) and (commonPoint, startPoint2)
         /// </summary>
         /// <param name="commonPoint"> Common point </param>
         /// <param name="startPoint1"> Start point of a segment 1 </param>
@@ -62,7 +62,7 @@ namespace GeoPlanarNet
         }
 
         /// <summary>
-        /// Get angle in radians between two segments: (commonPoint, startPoint1) and (commonPoint, startPoint2)
+        /// Get angle in radians between two segments with a common point: (commonPoint, startPoint1) and (commonPoint, startPoint2)
         /// </summary>
         /// <param name="commonPoint"> Common point </param>
         /// <param name="startPoint1"> Start point of a segment 1 </param>
@@ -74,7 +74,7 @@ namespace GeoPlanarNet
         }
 
         /// <summary>
-        /// Get angle in radians between two segments: (commonPoint, startPoint1) and (commonPoint, startPoint2)
+        /// Get angle in radians between two segments with a common point: (commonPoint, startPoint1) and (commonPoint, startPoint2)
         /// </summary>
         /// <param name="commonPointX"> Common point: X coordinate </param>
         /// <param name="commonPointY"> Common point: Y coordinate </param>
@@ -89,6 +89,57 @@ namespace GeoPlanarNet
             var denominator = Math.Sqrt(Math.Pow(startPoint1X - commonPointX, 2) + Math.Pow(startPoint1Y - commonPointY, 2)) * Math.Sqrt(Math.Pow(startPoint2X - commonPointX, 2) + Math.Pow(startPoint2Y - commonPointY, 2));
 
             return Math.Acos(numerator / denominator);
+        }
+
+        /// <summary>
+        /// Get angle in radians between two segments
+        /// </summary>
+        /// <param name="startPoint1"> Start point of 1 segment </param>
+        /// <param name="endPoint1"> End point of 1 segment </param>
+        /// <param name="startPoint2"> Start point of 2 segment </param>
+        /// <param name="endPoint2"> End point of 2 segment </param>
+        /// <returns> Angle (radians) </returns>
+        public static double GetAngleRadians(PointF startPoint1, PointF endPoint1, PointF startPoint2, PointF endPoint2)
+        {
+            return AngleBetweenVectors(startPoint1.X, startPoint1.Y, endPoint1.X, endPoint1.Y, startPoint2.X, startPoint2.Y, endPoint2.X, endPoint2.Y);
+        }
+
+        /// <summary>
+        /// Get angle in radians between two segments
+        /// </summary>
+        /// <param name="startPoint1"> Start point of 1 segment </param>
+        /// <param name="endPoint1"> End point of 1 segment </param>
+        /// <param name="startPoint2"> Start point of 2 segment </param>
+        /// <param name="endPoint2"> End point of 2 segment </param>
+        /// <returns> Angle (radians) </returns>
+        public static double GetAngleRadians(Point startPoint1, Point endPoint1, Point startPoint2, Point endPoint2)
+        {
+            return AngleBetweenVectors(startPoint1.X, startPoint1.Y, endPoint1.X, endPoint1.Y, startPoint2.X, startPoint2.Y, endPoint2.X, endPoint2.Y);
+        }
+
+        /// <summary>
+        /// Get angle in radians between two segments
+        /// </summary>
+        /// <param name="startPoint1X"> Start point of 1 segment: X coordinate </param>
+        /// <param name="startPoint1Y"> Start point of 1 segment: X coordinate </param>
+        /// <param name="endPoint1X"> Start point of 1 segment: X coordinate </param>
+        /// <param name="endPoint1Y"> Start point of 1 segment: X coordinate </param>
+        /// <param name="startPoint2X"> Start point of 1 segment: X coordinate </param>
+        /// <param name="startPoint2Y"> Start point of 1 segment: X coordinate </param>
+        /// <param name="endPoint2X"> Start point of 1 segment: X coordinate </param>
+        /// <param name="endPoint2Y"> Start point of 1 segment: X coordinate </param>
+        /// <returns> Angle (radians) </returns>
+        public static double AngleBetweenVectors(double startPoint1X, double startPoint1Y, double endPoint1X, double endPoint1Y,
+                                                 double startPoint2X, double startPoint2Y, double endPoint2X, double endPoint2Y)
+        {
+            var diff1X = endPoint1X - startPoint1X;
+            var diff1Y = endPoint1Y - startPoint1Y;
+            var diff2X = endPoint2X - startPoint2X;
+            var diff2Y = endPoint2Y - startPoint2Y;
+
+            var angle = ((diff1X * diff2X) + (diff1Y * diff2Y)) / (Math.Sqrt((diff1X * diff1X) + (diff1Y * diff1Y)) * Math.Sqrt((diff2X * diff2X) + (diff2Y * diff2Y)));
+
+            return Math.Acos(Math.Round(angle, 3));
         }
     }
 }

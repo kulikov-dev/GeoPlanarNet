66a929456c0a00b9fa4740f329a73c4d51f968cf
kulikov-dev <kulikov.dev@gmail.com>
2022-12-09 09:48:38 +0300

    Get area center point

--- GeoPlanarNet/AreaGeo.cs
+++ GeoPlanarNet/AreaGeo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace GeoPlanarNet
 {
@@ -14,15 +9,15 @@ namespace GeoPlanarNet
         /// </summary>
         /// <param name="points"> Area </param>
         /// <returns> Square </returns>
-        public static double Square(PointF[] points)
+        public static float Square(PointF[] points)
         {
-            double sum = 0;
+            var sum = 0f;
             for (var i = 1; i < points.Length; i++)
             {
                 sum += (points[i - 1].X * points[i].Y) - (points[i].X * points[i - 1].Y);
             }
 
-            return Math.Abs(sum / 2);
+            return Math.Abs(sum * 0.5f);
         }
 
         /// <summary>
@@ -39,6 +34,228 @@ namespace GeoPlanarNet
             }
 
             return Math.Abs(sum / 2);
+        }
+
+        /// <summary>
+        /// Get a minimum point of an area
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Minumum point </returns>
+        public static PointF GetMinPoint(PointF[] points)
+        {
+            if (points.Length == 0)
+            {
+                return PointF.Empty;
+            }
+
+            var result = new PointF(points[0].X, points[0].Y);
+
+            for (var i = 1; i < points.Length; i++)
+            {
+                var prevPoint = points[i - 1];
+                if (prevPoint.X < result.X)
+                {
+                    result.X = prevPoint.X;
+                }
+
+                if (prevPoint.Y < result.Y)
+                {
+                    result.Y = prevPoint.Y;
+                }
+
+                var currentPoint = points[i];
+                if (currentPoint.X < result.X)
+                {
+                    result.X = currentPoint.X;
+                }
+                if (currentPoint.Y < result.Y)
+                {
+                    result.Y = currentPoint.Y;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get a maximum point of an area
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Maximum point </returns>
+        public static PointF GetMaxPoint(PointF[] points)
+        {
+            if (points.Length == 0)
+            {
+                return PointF.Empty;
+            }
+
+            var result = new PointF(points[0].X, points[0].Y);
+
+            for (var i = 1; i < points.Length; i++)
+            {
+                var prevPoint = points[i - 1];
+
+                if (prevPoint.X > result.X)
+                {
+                    result.X = prevPoint.X;
+                }
+
+                if (prevPoint.Y > result.Y)
+                {
+                    result.Y = prevPoint.Y;
+                }
+
+                var currentPoint = points[i];
+
+                if (currentPoint.X > result.X)
+                {
+                    result.X = currentPoint.X;
+                }
+
+                if (currentPoint.Y > result.Y)
+                {
+                    result.Y = currentPoint.Y;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get a minimum point of an area
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Minumum point </returns>
+        public static Point GetMinPoint(Point[] points)
+        {
+            if (points.Length == 0)
+            {
+                return Point.Empty;
+            }
+
+            var result = new Point(points[0].X, points[0].Y);
+
+            for (var i = 1; i < points.Length; i++)
+            {
+                var prevPoint = points[i - 1];
+                if (prevPoint.X < result.X)
+                {
+                    result.X = prevPoint.X;
+                }
+
+                if (prevPoint.Y < result.Y)
+                {
+                    result.Y = prevPoint.Y;
+                }
+
+                var currentPoint = points[i];
+                if (currentPoint.X < result.X)
+                {
+                    result.X = currentPoint.X;
+                }
+                if (currentPoint.Y < result.Y)
+                {
+                    result.Y = currentPoint.Y;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get a maximum point of an area
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Maximum point </returns>
+        public static Point GetMaxPoint(Point[] points)
+        {
+            if (points.Length == 0)
+            {
+                return Point.Empty;
+            }
+
+            var result = new Point(points[0].X, points[0].Y);
+
+            for (var i = 1; i < points.Length; i++)
+            {
+                var prevPoint = points[i - 1];
+
+                if (prevPoint.X > result.X)
+                {
+                    result.X = prevPoint.X;
+                }
+
+                if (prevPoint.Y > result.Y)
+                {
+                    result.Y = prevPoint.Y;
+                }
+
+                var currentPoint = points[i];
+
+                if (currentPoint.X > result.X)
+                {
+                    result.X = currentPoint.X;
+                }
+
+                if (currentPoint.Y > result.Y)
+                {
+                    result.Y = currentPoint.Y;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get area center point
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Center point </returns>
+        public static PointF GetCenterPoint(PointF[] points)
+        {
+            var centerPoint = new PointF();
+            var maxPoint = GetMaxPoint(points);
+
+            var sumSquare = 0f;
+            for (var i = 0; i < points.Length; i++)
+            {
+                var currentSquare = (float)TriangleGeo.Square(maxPoint.X, maxPoint.Y, points[i].X, points[i].Y, points[(i + 1) % points.Length].X, points[(i + 1) % points.Length].Y);
+
+                centerPoint.X += currentSquare * (maxPoint.X + points[i].X + points[(i + 1) % points.Length].X) / 3;
+                centerPoint.Y += currentSquare * (maxPoint.Y + points[i].Y + points[(i + 1) % points.Length].Y) / 3;
+                sumSquare += currentSquare;
+            }
+
+            centerPoint.X /= sumSquare;
+            centerPoint.Y /= sumSquare;
+
+            return centerPoint;
+        }
+
+        /// <summary>
+        /// Get area center point
+        /// </summary>
+        /// <param name="points"> Area </param>
+        /// <returns> Center point </returns>
+        public static Point GetCenterPoint(Point[] points)
+        {
+            var centerPoint = new Point();
+            var maxPoint = GetMaxPoint(points);
+
+            var sumSquare = 0;
+            for (var i = 0; i < points.Length; i++)
+            {
+                var currentSquare = (int)TriangleGeo.Square(maxPoint.X, maxPoint.Y, points[i].X, points[i].Y, points[(i + 1) % points.Length].X, points[(i + 1) % points.Length].Y);
+
+                centerPoint.X += currentSquare * (maxPoint.X + points[i].X + points[(i + 1) % points.Length].X) / 3;
+                centerPoint.Y += currentSquare * (maxPoint.Y + points[i].Y + points[(i + 1) % points.Length].Y) / 3;
+                sumSquare += currentSquare;
+            }
+
+            centerPoint.X /= sumSquare;
+            centerPoint.Y /= sumSquare;
+
+            return centerPoint;
         }
     }
 }
--- GeoPlanarNet/TriangleGeo.cs
+++ GeoPlanarNet/TriangleGeo.cs
@@ -1,40 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace GeoPlanarNet
 {
     public static class TriangleGeo
     {
         /// <summary>
-        /// Get triangle area
+        /// Get triangle square
         /// </summary>
         /// <param name="apex1"> Apex 1 </param>
         /// <param name="apex2"> Apex 2 </param>
         /// <param name="apex3"> Apex 3 </param>
-        /// <returns> Triangle area </returns>
-        private static double Area(PointF apex1, PointF apex2, PointF apex3)
+        /// <returns> Triangle square </returns>
+        public static double Square(PointF apex1, PointF apex2, PointF apex3)
         {
-            return Area(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
+            return Square(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
         }
 
         /// <summary>
-        /// Get triangle area
+        /// Get triangle square
         /// </summary>
         /// <param name="apex1"> Apex 1 </param>
         /// <param name="apex2"> Apex 2 </param>
         /// <param name="apex3"> Apex 3 </param>
-        /// <returns> Triangle area </returns>
-        private static double Area(Point apex1, Point apex2, Point apex3)
+        /// <returns> Triangle square </returns>
+        public static double Square(Point apex1, Point apex2, Point apex3)
         {
-            return Area(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
+            return Square(apex1.X, apex1.Y, apex2.X, apex2.Y, apex3.X, apex3.Y);
         }
 
         /// <summary>
-        /// Get triangle area
+        /// Get triangle square
         /// </summary>
         /// <param name="apex1X"> Apex 1: X coordinate </param>
         /// <param name="apex1Y"> Apex 1: Y coordinate </param>
@@ -42,10 +37,10 @@ namespace GeoPlanarNet
         /// <param name="apex2Y"> Apex 2: Y coordinate </param>
         /// <param name="apex3X"> Apex 3: X coordinate </param>
         /// <param name="apex3Y"> Apex 3: Y coordinate </param>
-        /// <returns> Triangle area </returns>
-        private static double Area(double apex1X, double apex1Y, double apex2X, double apex2Y, double apex3X, double apex3Y)
+        /// <returns> Triangle square </returns>
+        public static double Square(double apex1X, double apex1Y, double apex2X, double apex2Y, double apex3X, double apex3Y)
         {
-            return 0.5d * (((apex2X - apex1X) * (apex3Y - apex1Y)) - ((apex2Y - apex1Y) * (apex3X - apex1X)));
+            return 0.5d * Math.Abs(((apex2X - apex3X) * (apex1Y - apex3Y)) - ((apex1X - apex3X) * (apex2Y - apex3Y)));
         }
     }
 }

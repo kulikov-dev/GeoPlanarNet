4e139e4f2198ea4a78e31fa4d67d1b0783a98ac7
kulikov-dev <kulikov.dev@gmail.com>
2022-12-12 13:37:10 +0300

    Get curve line length

---
+++ GeoPlanarNet/CurveLineGeo.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace GeoPlanarNet
+{
+    public class CurveLineGeo
+    {
+        /// <summary>
+        /// Get curve length
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <returns> Length </returns>
+        public static double Length(List<PointF> curve)
+        {
+            var length = 0d;
+
+            for (var i = 0; i < curve.Count - 1; ++i)
+            {
+                length += PointGeo.Distance(curve[i], curve[i + 1]);
+            }
+
+            return length;
+        }
+
+        /// <summary>
+        /// Get curve length
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <returns> Length </returns>
+        public static double Length(List<Point> curve)
+        {
+            var length = 0d;
+
+            for (var i = 0; i < curve.Count - 1; ++i)
+            {
+                length += PointGeo.Distance(curve[i], curve[i + 1]);
+            }
+
+            return length;
+        }
+    }
+}

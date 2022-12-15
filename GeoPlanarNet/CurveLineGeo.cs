241e03a948cd472116b12bd96617ebff79be891f
kulikov-dev <kulikov.dev@gmail.com>
2022-12-15 21:29:16 +0300

    Find the minimal distance from a curve to a point

--- GeoPlanarNet/CurveLineGeo.cs
+++ GeoPlanarNet/CurveLineGeo.cs
@@ -37,5 +37,89 @@ namespace GeoPlanarNet
 
             return length;
         }
+
+        /// <summary>
+        /// Find the minimal distance from a curve to a point
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <param name="point"> Point </param>
+        /// <returns> Minimal distance </returns>
+        public static double MinDistanceToPoint(PointF[] curve, PointF point)
+        {
+            return MinDistanceToPoint(curve, point, out _);
+        }
+
+        /// <summary>
+        /// Find the minimal distance from a curve to a point
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <param name="point"> Point </param>
+        /// <param name="nearestPointIndex"> Index of a nearest point in the curve </param>
+        /// <returns> Minimal distance </returns>
+        public static double MinDistanceToPoint(PointF[] curve, PointF point, out int nearestPointIndex)
+        {
+            var resultLength = double.MaxValue;
+            nearestPointIndex = -1;
+
+            if (curve.Length == 1)
+            {
+                resultLength = PointGeo.Distance(point.X, point.Y, curve[0].X, curve[0].Y);
+            }
+
+            for (var i = 0; i < curve.Length - 1; i++)
+            {
+                var currentLength = PointGeo.DistanceToSegment(point.X, point.Y, curve[i].X, curve[i].Y, curve[i + 1].X, curve[i + 1].Y);
+
+                if (currentLength < resultLength)
+                {
+                    resultLength = currentLength;
+                    nearestPointIndex = i;
+                }
+            }
+
+            return resultLength;
+        }
+
+        /// <summary>
+        /// Find the minimal distance from a curve to a point
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <param name="point"> Point </param>
+        /// <returns> Minimal distance </returns>
+        public static double MinDistanceToPoint(Point[] curve, Point point)
+        {
+            return MinDistanceToPoint(curve, point, out _);
+        }
+
+        /// <summary>
+        /// Find the minimal distance from a curve to a point
+        /// </summary>
+        /// <param name="curve"> Curve </param>
+        /// <param name="point"> Point </param>
+        /// <param name="nearestPointIndex"> Index of a nearest point in the curve </param>
+        /// <returns> Minimal distance </returns>
+        public static double MinDistanceToPoint(Point[] curve, Point point, out int nearestPointIndex)
+        {
+            var resultLength = double.MaxValue;
+            nearestPointIndex = -1;
+
+            if (curve.Length == 1)
+            {
+                resultLength = PointGeo.Distance(point.X, point.Y, curve[0].X, curve[0].Y);
+            }
+
+            for (var i = 0; i < curve.Length - 1; i++)
+            {
+                var currentLength = PointGeo.DistanceToSegment(point.X, point.Y, curve[i].X, curve[i].Y, curve[i + 1].X, curve[i + 1].Y);
+
+                if (currentLength < resultLength)
+                {
+                    resultLength = currentLength;
+                    nearestPointIndex = i;
+                }
+            }
+
+            return resultLength;
+        }
     }
 }

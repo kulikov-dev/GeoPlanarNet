a0807a2f5551259b389f8ded5e89c0a61b99ba2d
kulikov-dev <kulikov.dev@gmail.com>
2022-12-22 12:20:31 +0300

    Get circle area

---
+++ GeoPlanarNet/CircleGeo.cs
@@ -0,0 +1,15 @@
+﻿namespace GeoPlanarNet
+{
+    public static class CircleGeo
+    {
+        /// <summary>
+        /// Get the circle area
+        /// </summary>
+        /// <param name="radius"> Radius </param>
+        /// <returns> Area </returns>
+        public static double Area(double radius)
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+}

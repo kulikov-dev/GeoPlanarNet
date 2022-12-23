a05ea4b4b9cc8543e5fe085c74f5228ae687944e
kulikov-dev <kulikov.dev@gmail.com>
2022-12-23 13:47:26 +0300

    Get the point on the circle edge on the specified angle (radians)

--- GeoPlanarNet/CircleGeo.cs
+++ GeoPlanarNet/CircleGeo.cs
@@ -1,4 +1,6 @@
-﻿namespace GeoPlanarNet
+﻿using System.Drawing;
+
+namespace GeoPlanarNet
 {
     public static class CircleGeo
     {
@@ -10,6 +12,34 @@
         public static double Area(double radius)
         {
             return radius * radius * Math.PI;
+        }
+
+        /// <summary>
+        /// Get the point on the circle edge on the specified angle (radians)
+        /// </summary>
+        /// <param name="pointCenter"> Center point </param>
+        /// <param name="radius"> Radius </param>
+        /// <param name="angleRad"> Angle (radians) </param>
+        /// <returns> Point on the circle edge </returns>
+        public static PointF GetEdgePoint(PointF pointCenter, double radius, double angleRad)
+        {
+            GetEdgePoint(pointCenter.X, pointCenter.Y, radius, angleRad, out double x, out double y);
+            return new PointF((float)x, (float)y);
+        }
+
+        /// <summary>
+        /// Get the point on the circle edge on the specified angle (radians)
+        /// </summary>
+        /// <param name="pointCenterX"> Center point: X coordinate </param>
+        /// <param name="pointCenterY"> Center point: Y coordinate </param>
+        /// <param name="radius"> Radius </param>
+        /// <param name="angleRad"> Angle (radians) </param>
+        /// <param name="edgePointX"> Point on the edge: X coordinate </param>
+        /// <param name="edgePointY"> Point on the edge: Y coordinate </param>
+        public static void GetEdgePoint(double pointCenterX, double pointCenterY, double radius, double angleRad, out double edgePointX, out double edgePointY)
+        {
+            edgePointX = pointCenterX + radius * Math.Cos(angleRad);
+            edgePointY = pointCenterY + radius * Math.Sin(angleRad);
         }
     }
 }

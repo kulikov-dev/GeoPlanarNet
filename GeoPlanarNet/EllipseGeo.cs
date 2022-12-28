bcf0680450bd9037b82aa3a647393d886c9da07a
kulikov-dev <kulikov.dev@gmail.com>
2022-12-28 17:06:23 +0300

    Get the ellipse eccentricity

--- GeoPlanarNet/EllipseGeo.cs
+++ GeoPlanarNet/EllipseGeo.cs
@@ -10,38 +10,76 @@ namespace GeoPlanarNet
         /// <summary>
         /// Get the ellipse area
         /// </summary>
-        /// <param name="ellipseCenter"> Ellipse center </param>
         /// <param name="radiusX"> Radius on X axis </param>
         /// <param name="radiusY"> Radius on Y axis </param>
         /// <returns> Area </returns>
-        public static double Area(PointF ellipseCenter, float radiusX, float radiusY)
-        {
-            return Area(ellipseCenter.X, ellipseCenter.Y, radiusX, radiusY);
-        }
-
-        /// <summary>
-        /// Get the ellipse area
-        /// </summary>
-        /// <param name="ellipseCenter"> Ellipse center </param>
-        /// <param name="radiusX"> Radius on X axis </param>
-        /// <param name="radiusY"> Radius on Y axis </param>
-        /// <returns> Area </returns>
-        public static double Area(Point ellipseCenter, int radiusX, int radiusY)
-        {
-            return Area(ellipseCenter.X, ellipseCenter.Y, radiusX, radiusY);
-        }
-
-        /// <summary>
-        /// Get the ellipse area
-        /// </summary>
-        /// <param name="ellipseCenterX"> Ellipse center: X coordinate </param>
-        /// <param name="ellipseCenterY"> Ellipse center: Y coordinate </param>
-        /// <param name="radiusX"> Radius on X axis </param>
-        /// <param name="radiusY"> Radius on Y axis </param>
-        /// <returns> Area </returns>
-        public static double Area(double ellipseCenterX, double ellipseCenterY, double radiusX, double radiusY)
+        public static double Area(float radiusX, float radiusY)
         {
             return Math.PI * radiusX * radiusY;
+        }
+
+        /// <summary>
+        /// Get the ellipse area
+        /// </summary>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Area </returns>
+        public static double Area(int radiusX, int radiusY)
+        {
+            return Math.PI * radiusX * radiusY;
+        }
+
+        /// <summary>
+        /// Get the ellipse area
+        /// </summary>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Area </returns>
+        public static double Area(double radiusX, double radiusY)
+        {
+            return Math.PI * radiusX * radiusY;
+        }
+
+        /// <summary>
+        /// Get the ellipse eccentricity
+        /// </summary>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Eccentricity </returns>
+        public static double Eccentricity(float radiusX, float radiusY)
+        {
+            var semiMajor = radiusX * radiusX;
+            var semiMinor = radiusY * radiusY;
+
+            return Math.Sqrt(1 - semiMinor / semiMajor);
+        }
+
+        /// <summary>
+        /// Get the ellipse eccentricity
+        /// </summary>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Eccentricity </returns>
+        public static double Eccentricity(int radiusX, int radiusY)
+        {
+            var semiMajor = radiusX * radiusX;
+            var semiMinor = radiusY * radiusY;
+
+            return Math.Sqrt(1 - semiMinor / semiMajor);
+        }
+
+        /// <summary>
+        /// Get the ellipse eccentricity
+        /// </summary>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Eccentricity </returns>
+        public static double Eccentricity(double radiusX, double radiusY)
+        {
+            var semiMajor = radiusX * radiusX;
+            var semiMinor = radiusY * radiusY;
+
+            return Math.Sqrt(1 - semiMinor / semiMajor);
         }
     }
 }

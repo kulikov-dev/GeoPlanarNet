6c0ab876aa2784a92b374e6c2259cc5e040eb8e1
kulikov-dev <kulikov.dev@gmail.com>
2022-12-28 16:59:47 +0300

    Ellipse area

---
+++ GeoPlanarNet/EllipseGeo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GeoPlanarNet
+{
+    public static class EllipseGeo
+    {
+        /// <summary>
+        /// Get the ellipse area
+        /// </summary>
+        /// <param name="ellipseCenter"> Ellipse center </param>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Area </returns>
+        public static double Area(PointF ellipseCenter, float radiusX, float radiusY)
+        {
+            return Area(ellipseCenter.X, ellipseCenter.Y, radiusX, radiusY);
+        }
+
+        /// <summary>
+        /// Get the ellipse area
+        /// </summary>
+        /// <param name="ellipseCenter"> Ellipse center </param>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Area </returns>
+        public static double Area(Point ellipseCenter, int radiusX, int radiusY)
+        {
+            return Area(ellipseCenter.X, ellipseCenter.Y, radiusX, radiusY);
+        }
+
+        /// <summary>
+        /// Get the ellipse area
+        /// </summary>
+        /// <param name="ellipseCenterX"> Ellipse center: X coordinate </param>
+        /// <param name="ellipseCenterY"> Ellipse center: Y coordinate </param>
+        /// <param name="radiusX"> Radius on X axis </param>
+        /// <param name="radiusY"> Radius on Y axis </param>
+        /// <returns> Area </returns>
+        public static double Area(double ellipseCenterX, double ellipseCenterY, double radiusX, double radiusY)
+        {
+            return Math.PI * radiusX * radiusY;
+        }
+    }
+}
